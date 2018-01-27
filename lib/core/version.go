1b8d5c95d3a5fc51ff6a5ac8f554f120c7135258
enr <enrico.comiti@gmail.com>
2018-01-28 00:09:37 +0100

    doc

--- lib/core/version.go
+++ lib/core/version.go
@@ -1,10 +1,10 @@
 package core
 
-// version of the running app.
+// Version of the running app.
 var Version = ""
 
-// buildTime of the running app.
+// BuildTime of the running app.
 var BuildTime = ""
 
-// gitCommit is the Git hash and a "+CHANGES" suffix if there are uncommitted changes.
+// GitCommit is the Git hash and a "+CHANGES" suffix if there are uncommitted changes.
 var GitCommit = ""
