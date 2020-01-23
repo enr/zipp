6ee6667a6e4e3064f31afe52b760d8facb45f45e
enr <enrico.comiti@gmail.com>
2020-01-23 23:44:03 +0100

    windows ci

--- .sdlc/check.cmd
+++ .sdlc/check.cmd
@@ -11,34 +11,32 @@ cd ..
 set project_dir=%cd%
 
 set module_name=%REPO_HOST%/%REPO_OWNER%/%REPO_NAME%
-set exe_path=bin\%REPO_NAME%.exe
 
 echo script_name   %script_name%
 echo script_path   %script_path%
 echo script_dir    %script_dir%
 echo project_dir   %project_dir%
 echo module_name   %module_name%
-echo exe_path      %exe_path%
 
 cd %project_dir%
 
 for /f %%x in ('dir /AD /B /S lib') do (
-    echo --- %%x
+    echo --- go test lib %%x
     cd %%x
     go test -mod vendor -cover ./...
 )
 
 cd %project_dir%
 
-IF EXIST %exe_path% DEL /F %exe_path%
+REM IF EXIST %exe_path% DEL /F %exe_path%
 
 @echo ON
-call go build -mod vendor -ldflags "-s -X %module_name%/lib/core.Version=%APP_VERSION% -X %module_name%/lib/core.BuildTime=%TIMESTAMP% -X %module_name%/lib/core.GitCommit=win-dev-commit" ^
-  -o %exe_path% "%module_name%/cmd/%REPO_NAME%"
-
-@echo OFF
 for /f %%x in ('dir /AD /B /S cmd') do (
-    echo --- %%x
+    echo --- go test cmd %%x
     cd %%x
+    set bin_path=%%~nx
+    set exe_path=%bin_path%.exe
+    call go build -mod vendor -ldflags "-s -X %module_name%/lib/core.Version=%APP_VERSION% -X %module_name%/lib/core.BuildTime=%TIMESTAMP% -X %module_name%/lib/core.GitCommit=win-dev-commit" ^
+    -o %exe_path% ./...
     go test -mod vendor -cover ./...
 )
--- .sdlc/config.bat
+++ .sdlc/config.bat
@@ -1,6 +1,6 @@
 set REPO_HOST=github.com
 set REPO_OWNER=enr
-set REPO_NAME=gip
+set REPO_NAME=zipp
 set TIMESTAMP=win-dev-timestamp
 
 set GOPROXY='off'
