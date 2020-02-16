a83cd407f30a759ba68b1b880e7048e39e10636f
enr <enrico.comiti@gmail.com>
2020-02-16 16:10:44 +0100

    tests

---
+++ lib/qac/models_test.go
@@ -0,0 +1,16 @@
+package qac
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestConventionalToFullSpec(t *testing.T) {
+	conventional := ConventionalSpec{
+		CommandExe: "testdata/executable",
+	}
+	spec := toFullSpec(conventional)
+	if !strings.Contains(spec.Command.Exe, "testdata/executable") {
+		t.Errorf("Expected command exe containing <testdata/executable> but got <%s>", spec.Command.Exe)
+	}
+}
