93017606f7e4096266e552b18653a3adebf58f72
enr <enrico.comiti@gmail.com>
2020-02-27 23:10:49 +0100

    ci windows

--- .sdlc/build.cmd
+++ .sdlc/build.cmd
@@ -27,3 +27,13 @@ IF EXIST %exe_path% DEL /F %exe_path%
 @echo ON
 call go build -mod vendor -ldflags "-s -X %module_name%/lib/core.Version=%APP_VERSION% -X %module_name%/lib/core.BuildTime=%TIMESTAMP% -X %module_name%/lib/core.GitCommit=win-dev-commit" ^
   -o %exe_path% "%module_name%/cmd/%REPO_NAME%"
+
+@echo ON
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /f %%x in ('dir /AD /B /S cmd') do (
+    echo --- go build cmd %%x
+    cd %%x
+    set bin_name=%%~nx
+    call go build -mod vendor -ldflags "-s -X %module_name%/lib/core.Version=%APP_VERSION% -X %module_name%/lib/core.BuildTime=%TIMESTAMP% -X %module_name%/lib/core.GitCommit=win-dev-commit" ^
+    -o %bin_dir%\!bin_name!.exe "%module_name%/cmd/!bin_name!"
+)
