d263338c42ea56c58e5f0e3d634cab450049b6f9
enr <enrico.comiti@gmail.com>
2021-02-28 17:42:28 +0100

    fix ci

--- lib/core/timestamp.go
+++ lib/core/timestamp.go
@@ -2,6 +2,7 @@ package core
 
 import "time"
 
+// Timestamp return a string representation of the timestamp in format "yyyymmddhhmmss"
 func Timestamp() string {
 	const layout = "20060102150405"
 	t := time.Now()
